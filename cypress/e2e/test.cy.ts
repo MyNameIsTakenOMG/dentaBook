519b1e0a147d0f60bf14f5191b8ea784bd7afc95
Sean Fang <fangzhengonly@gmail.com>
2023-09-29 17:38:29 -0400

    update test suite

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -1,6 +1,10 @@
-describe('template spec', () => {
+describe('test spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000');
     cy.contains(/this is home page/i).should('exist');
+  });
+  it('passes', () => {
+    cy.visit('http://localhost:3000');
+    cy.contains(/home page/i).should('exist');
   });
 });
