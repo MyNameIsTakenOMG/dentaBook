42e600577f1c5d4a40ade0b01d9c08825e198f11
Sean Fang <fangzhengonly@gmail.com>
2023-09-29 16:14:56 -0400

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,10 @@ import Image from 'next/image';
 import styles from './page.module.css';
 
 export default function Home() {
-  return <main className={styles.main}>this is home page</main>;
+  return (
+    <main className={styles.main}>
+      this is home page
+      <p>test if amplify cicd cypress setup already</p>
+    </main>
+  );
 }
